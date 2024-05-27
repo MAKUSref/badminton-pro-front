bb6e326a56582588f5fdc9b74ac83633d8798d04
Tomek <tomaszmakowski002731@gmail.com>
2024-05-27 13:03:11 +0200

    Add settings page

---
+++ src/components/SettingsSection/index.tsx
@@ -0,0 +1,55 @@
+import { Box, Button, Divider, Typography } from '@mui/material';
+import { PropsWithChildren } from 'react';
+
+const BORDER_COLOR = '#0000001f';
+const BG_COLOR_DANGER = '#ffeaea';
+
+interface SettingsSectionProps {
+  title: string;
+  description: string;
+  onSave?: () => void;
+  sectionVariant?: 'primary' | 'danger';
+  btnLabel?: string;
+}
+
+const SettingsSection = ({
+  description,
+  onSave,
+  title,
+  children,
+  sectionVariant = 'primary',
+  btnLabel = 'Zapisz'
+}: PropsWithChildren<SettingsSectionProps>) => {
+  return (
+    <Box
+      border="1px solid"
+      borderRadius={2}
+      borderColor={sectionVariant === 'primary' ? BORDER_COLOR : 'red'}
+      pt={3}
+      mb={4}>
+      <Box px={3}>
+        <Typography mb={2} variant="h3" fontSize="1.4rem" fontWeight="500">
+          {title}
+        </Typography>
+        <Typography mb={1}>{description}</Typography>
+        {children}
+      </Box>
+      <Divider sx={{ borderColor: sectionVariant === 'primary' ? BORDER_COLOR : 'red' }} />
+      <Box
+        sx={{ borderBottomLeftRadius: 8, borderBottomRightRadius: 8 }}
+        px={3}
+        py={1}
+        bgcolor={sectionVariant === 'primary' ? 'transparent' : BG_COLOR_DANGER}>
+        <Button
+          sx={{ ml: 'auto', display: 'block' }}
+          variant="contained"
+          color={sectionVariant === 'primary' ? 'primary' : 'error'}
+          onClick={onSave}>
+          {btnLabel}
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default SettingsSection;
--- src/pages/App/SettingsPage/index.tsx
+++ src/pages/App/SettingsPage/index.tsx
@@ -1,12 +1,33 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, TextField, Typography } from '@mui/material';
+
+import SettingsSection from '@/components/SettingsSection';
 
 const SettingsPage = () => {
   return (
     <Container>
-      <Typography variant="h2" my={2}>
+      <Typography variant="h2" mt={2} mb={4}>
         Ustawienia Turnieju
       </Typography>
-      <Box></Box>
+      <SettingsSection
+        title="Nazwa turnieju"
+        description="Reprezentatywna nazwa turnieju, którą będą widzieć wszyscy użytkownicy. Po tej nazwie
+            powinien być rozpoznawany turniej.">
+        <Box my={2}>
+          <TextField size="small" label="Nazwa turnieju" />
+        </Box>
+      </SettingsSection>
+      <SettingsSection
+        title="Opis turnieju"
+        description="Ten tekst pojawi się każdemu użytkownikowi pod nazwą turnieju.">
+        <Box my={2} maxWidth="650px">
+          <TextField size="small" label="Opis turnieju" fullWidth />
+        </Box>
+      </SettingsSection>
+      <SettingsSection
+        sectionVariant="danger"
+        btnLabel="Usuń turniej"
+        title="Usuń turniej"
+        description="Ten turniej zostanie permamentie usunięty. Również z historii."></SettingsSection>
     </Container>
   );
 };
