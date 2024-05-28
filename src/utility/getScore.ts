5edecdb5115ad57562d3c9657feb3a2032fb9549
kasia251011 <katarzyna.jankowska000728@gmail.com>
2024-05-28 14:00:10 +0200

    Fix 0s set points bug

--- src/utility/getScore.ts
+++ src/utility/getScore.ts
@@ -5,9 +5,10 @@ interface SetScoreProps {
 }
 
 export const getScore = ({ participation: { set1, set2, set3 } }: SetScoreProps) => {
-  if (!set1 || !set2) return '-';
+  if (set1 == null || set2 == null) return '-';
   if (set1 === 21 && set2 === 21) return '3';
   else if ((set1 === 21 && set3 === 21) || (set2 === 21 && set3 === 21)) return '2';
-  else if ((set3 && set1 === 21 && set3 < 21) || (set3 && set2 === 21 && set3 < 21)) return '1';
+  else if ((set3 != null && set1 === 21 && set3 < 21) || (set3 != null && set2 === 21 && set3 < 21))
+    return '1';
   else return '0';
 };
